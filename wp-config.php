b77c940e94808c3c771b976d24ffc002be86ce34
kusamax <kpc1000939@kosei-shuppan.co.jp>
2016-07-21 17:13:29 +0900

    Revert "Update wp-config.php"
    This reverts commit 49fb88d196c1d95cad7147b33ee77aefafe66cc6.

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,3 @@
-/*
-Editor SSL forwading
-*/
-
 define('FORCE_SSL_ADMIN', true);
 if ( ! empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
        $_SERVER['HTTPS']='on';
