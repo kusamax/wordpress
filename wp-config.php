49fb88d196c1d95cad7147b33ee77aefafe66cc6
kusamax <kusamax1981@gmail.com>
2016-07-21 17:09:00 +0900

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,7 @@
+/*
+Editor SSL forwading
+*/
+
 define('FORCE_SSL_ADMIN', true);
 if ( ! empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
        $_SERVER['HTTPS']='on';
